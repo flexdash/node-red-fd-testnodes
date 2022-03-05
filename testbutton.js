11d70d5124faa34c5f3266951edbbc08a89db9c5
Thorsten von Eicken <tve@voneicken.com>
2022-03-04 18:31:46 -0800

    working gauge and button

--- testbutton.js
+++ testbutton.js
@@ -1,4 +1,4 @@
-// FlexDash Widget nodes for Node-RED
+// FlexDash button for Node-RED
 // Copyright (c) 2022 by Thorsten von Eicken, see LICENSE
 
 module.exports = function (RED) {
@@ -6,11 +6,28 @@ module.exports = function (RED) {
   // flexdashTestButton instatiates a FD Widget TestButton node, this is the node being constructed
   // and config are the values set by the user in the flow editor.
   function flexdashTestButton(config) {
-    const fd = RED.nodes.getNode(config.server)
+    const fd = RED.nodes.getNode(config.fd)
     RED.nodes.createNode(this, config)
 
-    this.on("input", msg => {
+    // propagate this node's config to the FD widget
+    // The third arg is the kind of widget to create, if it doesn't exist
+    fd.initWidget(this, config, 'PushButton')
 
+    // handle flow input messages, basically massage them a bit and update the FD widget
+    this.on("input", msg => {
+      // prepare update of widget params
+      const params = typeof msg.params === 'object' ? Object.assign({}, msg.params) : {}
+      // msg.payload is interpreted as setting the button value, i.e. params.value
+      if ('payload' in msg) params.value = msg.payload
+      // send the params to the widget
+      fd.updateWidget(this, params)
+    })
+
+    // handle widget input messages, we receive the payload sent by the widget
+    fd.onInput(this, payload => {
+      // the button sends the value of its output_value param, we just propagate it as msg.payload
+      // note that output_value could be an object, so we could decide to use that as full msg...
+      this.send({payload})
     })
 
   }
--- testgauge.js
+++ testgauge.js
@@ -1,19 +1,29 @@
-// FlexDash Widget nodes for Node-RED
+// FlexDash gauge for Node-RED
 // Copyright (c) 2022 by Thorsten von Eicken, see LICENSE
 
 module.exports = function (RED) {
 
-  // flexdashTestButton instatiates a FD Widget TestButton node, this is the node being constructed
+  // flexdashTestGauge instatiates a FD Widget TestGauge node, 'this' is the node being constructed
   // and config are the values set by the user in the flow editor.
-  function flexdashTestButton(config) {
-    const fd = RED.nodes.getNode(config.server)
+  function flexdashTestGauge(config) {
+    const fd = RED.nodes.getNode(config.fd) // get a handle onto FlexDash
     RED.nodes.createNode(this, config)
 
-    this.on("input", msg => {
+    // propagate this node's config to the FD widget
+    // The third arg is the kind of widget to create, if it doesn't exist
+    fd.initWidget(this, config, 'Gauge')
 
+    // handle flow input messages, basically massage them a bit and update the FD widget
+    this.on("input", msg => {
+      // prepare update of widget params
+      const params = typeof msg.params === 'object' ? Object.assign({}, msg.params) : {}
+      // msg.payload is interpreted as setting the gauge value, i.e. params.value
+      if ('payload' in msg) params.value = msg.payload
+      // send the params to the widget
+      fd.updateWidget(this, params)
     })
 
   }
 
-  RED.nodes.registerType("flexdash testbutton", flexdashTestButton)
+  RED.nodes.registerType("flexdash testgauge", flexdashTestGauge)
 }
